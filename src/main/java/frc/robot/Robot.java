e66c04dd8bf2fb8963362c15f6143a5ceeedce23
Spencer <aklixio@gmail.com>
2021-08-27 10:54:28 -0400

    Enable Compressor

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.Compressor;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -80,7 +81,16 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    Compressor c = new Compressor(0);
+
+    c.setClosedLoopControl(true);
+    c.setClosedLoopControl(false);
+
+    boolean enabled = c.enabled();
+    boolean pressureSwitch = c.getPressureSwitchValue();
+    double current = c.getCompressorCurrent();
+  }
 
   @Override
   public void testInit() {
